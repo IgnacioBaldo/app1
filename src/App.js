9c2e4929fc6666d35472fcad67d1cf62dc54977f
Ignacio <ignaciobaldo@live.com.ar>
2022-01-19 12:46:15 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import jsFundamental from './js-fundamental';
+import Navbar from './Components/navBar'
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
      <h1>Hola CoderHouse!</h1>
     </div>
   );
